729cabc8e9ae768a29b89518b308ac1b89f28a75
Reayah Bravo <reayahbravo@gmail.com>
2018-07-13 17:42:48 -0700

    Commit befor pushing week6_homework

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
     #i added both of the following lines
-    #before_action :authenticate_user!, only: [:new, :create, :destroy, :update]
-    #before_action :authorize_user!, only: [:edit, :update, :destroy]
+    before_action :authenticate_user!, only: [:new, :create, :destroy, :update]
+    # before_action :authorize_user!, only: [:edit, :update, :destroy]
 
     def new
       @user = User.new
@@ -39,42 +39,108 @@ class UsersController < ApplicationController
 
     def update
         @user = User.find params[:id]
+        # params.require(:user).permit(:first_name, :last_name, :current_password, :new_password, :new_password_confirmation)
         # raise @user.inspect
-        if @user.update edit_user_params
-            flash[:notice] = 'Your information has been updated!'
-            # redirect_to @user
-            # user = User.find params[:id]
-            # user.update(user_params)
-         
-          else
-            flash[:alert] = 'Your information did not update.'
-            # render :edit
-          end
-          redirect_to edit_user_path(@user)
-        end
+        current_password = params[:user][:current_password]
+        new_password = params[:user][:new_password]
+        new_password_confirmation = params[:user][:new_password_confirmation]
+        params[:user].delete(:current_password)
+        params[:user].delete(:new_password)
+        params[:user].delete(:new_password_confirmation)
+        user = current_user.authenticate(current_password)
+        if new_password != new_password_confirmation
+            flash[:alert] = 'Your passwords do not match!'
+            redirect_to edit_user_path(@user)
+        elsif user && new_password.present? && new_password == new_password_confirmation
+            # @user = User.new(first_name: first_name, last_name: last_name, email: email, password: new_password)
+            if @user.update(edit_user_params) && @user.reload.update(password: new_password)
+                flash[:success] = 'Your password and details have been updated!'
+                redirect_to edit_user_path(@user)
+            else
+                flash[:alert] = 'Something went wrong!'
+                redirect_to edit_user_path(@user)
+            end
 
-    #also def update_password block all me
-    def update_password
-        @user = current_user
-        if @user.update(user_params)
-            #sign in the user by passing validation in case their password changed
-            bypass_sign_in(@user)
-            redirect_to_root_path
-        else
-            render :edit
+        elsif user
+            # @user = User.new(first_name: first_name, last_name: last_name, email: email, password: new_password)
+            if @user.update(edit_user_params)
+                flash[:success] = 'Your information has been updated!'
+                redirect_to edit_user_path(@user)
+            else
+                flash[:alert] = 'Something went wrong!'
+                redirect_to edit_user_path(@user)
+            end
+        else 
+            flash[:alert] = 'Your current password does not match the existing one!'
+            redirect_to edit_user_path(@user)
         end
     end
+
+    def update_password
+        #\ raise @user.inspect
+        @user = User.find params[:id]
+        # @user = current_user
+        current_password = params[:user][:current_password]
+        new_password = params[:user][:new_password]
+        user = current_user&.authenticate(current_password)
+        # if @user.update(user_params)
+        #     #sign in the user by passing validation in case their password changed
+        #     bypass_sign_in(@user)
+        #     redirect_to_root_path
+        if new_password == params[:user][:new_password_confirmation]
+           @user.update(password: new_password)
+           flash[:success] = "Password Updated"
+           redirect_to edit_user_path
+        else
+            flash[:alert] = "Passwords Do Not Match"
+            redirect_to password_edit_user_path
+            # render :edit
+        end
+    end 
+
+    def password_params
+            params.require(:user).permit(
+             :current_password,
+             :new_password,
+             :new_password_confirmation
+            )
+    end
+
+    # def pw_update
+    #     current_password = params[:user][:current_password]
+    #     new_password = params[:user][:new_password]
+    #     user = current_user&.authenticate(current_password)
+    #     if @user && user
+    #      if new_password == params[:user][:new_password_confirmation]
+    #        @user.update(password: new_password)
+    #        flash[:success] = "Password Updated"
+    #        redirect_to edit_user_path
+    #      else
+    #       flash[:alert] = "Passwords Do Not Match"
+    #       redirect_to pw_edit_user_path
+    #      end
+    #    else
+    #     flash[:alert] = "Current Password is Incorrect"
+    #     redirect_to pw_edit_user_path
+    #    end
+    #  end def pw_params
+    #      params.require(:user).permit(
+    #          :current_password,
+    #          :new_password,
+    #          :new_password_confirmation
+    #          )
+    #  end
 
     private
     def edit_user_params
         params.require(:user).permit(
-            :first_name, :last_name, :email 
+            :first_name, :last_name, :email, :current_password, :new_password, :new_password_confirmation
         )
     end
 
     def user_params
       params.require(:user).permit(
-        :first_name, :last_name, :email, :password, :password_confirmation
+        :first_name, :last_name, :email, :password
       )
     end
 end
